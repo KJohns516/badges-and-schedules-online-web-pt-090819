7591998845e59c39d25bcff61df92529ba79ce5d
Kristan Johns <kristandjohns@gmail.com>
2019-09-19 20:15:48 +0000

    Automatically backed up by Learn

--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -57,8 +57,7 @@ TEXT
 
     it 'should return a formatted badge' do
       expect(badge_maker(name)).to eq("Hello, my name is #{name}.")
-      each_with_index.return
-      assign_rooms.return
+      batch_badge_creator.return
     end
 
   end
